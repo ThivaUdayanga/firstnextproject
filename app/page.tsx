eb2aa27a260d14d14229461f1bffb711e2fe4b9d
Thivanka Wimalasena <thivanka.wimalasena@gmail.com>
2026-02-24 00:31:48 +0530

    Next Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <ul>
+        <li><Link href='/'>Home</Link></li>
+        <li><Link href='/services'>Services</Link></li>
+        <li><Link href='/products'>Products</Link></li>
+        <li><Link href='/about'>About us</Link></li>
+        <li><Link href='/contact'>Contact Us</Link></li>
+      </ul>
     </>
   );
 }
